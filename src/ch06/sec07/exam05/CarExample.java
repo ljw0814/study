1e9a46e71ade10800eea81dc4330f543e31fcdc8
ljw0814 <leejeongwon8145@gmail.com>
2024-02-17 18:15:36 +0900

    매개변수와 필드(멤버변수)의 차이와, 오버로딩의 로직을 알아보았다

--- src/ch06/sec07/exam05/CarExample.java
+++ src/ch06/sec07/exam05/CarExample.java
@@ -2,7 +2,10 @@ package ch06.sec07.exam05;
 
 public class CarExample {
 	public static void main(String[] args) {
+		// 1. car 클래스에 "현대자동차"매개변수로 생성자를 찾는다
+		
 		Car car1 = new Car("현대자동차");
+		// 6. Car클래스의 company클래스 변수를 출력한다.
 		System.out.println(car1.company);
 		System.out.println(car1.color);
 		System.out.println(car1.price);
